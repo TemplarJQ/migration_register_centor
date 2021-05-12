5ce07229af1cd67e12c7fa6b5a84a3972ea57b19
TemplarJQ <jqliu_bupt@163.com>
2021-05-12 20:44:07 +0800

    base eureka register centor

---
+++ src/main/java/com/EurekaApplication.java
@@ -0,0 +1,21 @@
+package com;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+/**
+ * 注册中心启动!
+ *
+ */
+
+@EnableEurekaServer
+@SpringBootApplication
+public class EurekaApplication
+{
+    public static void main( String[] args )
+    {
+        SpringApplication.run(EurekaApplication.class, args);
+        System.out.println( "Eureka Register Running!" );
+    }
+}
---
+++ src/main/java/com/dao/Node.java
@@ -0,0 +1,19 @@
+package com.dao;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class Node {
+
+    public String nodeID;
+
+    public String nodeState;
+
+    public Integer nodeLevel;
+}
