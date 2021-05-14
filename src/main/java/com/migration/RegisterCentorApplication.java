bc84e8a8c5937f1858c2016a40ab874c16d20b9c
TemplarJQ <jqliu_bupt@163.com>
2021-05-14 16:52:53 +0800

    fix pom

--- src/main/java/com/migration/RegisterCentorApplication.java
+++ src/main/java/com/migration/RegisterCentorApplication.java
@@ -1,3 +1,5 @@
+package com.migration;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
